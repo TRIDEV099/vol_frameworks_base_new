9ac5401747f779ba1885873a3f4d54d7529968f8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-21 21:55:28 +0800

    SystemUI: Add edge light feature
    @neobuddy89:
    * Fix multi-user usage.
    * Define settings key we used earlier.
    
    Change-Id: I2bdf550eadd92d5f914b11ac48101eb8cf368e97
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7307,6 +7307,24 @@ public final class Settings {
         public static final String LS_MEDIA_ART_BLUR_LEVEL = "ls_media_art_blur_level";
 
         /**
+         * Whether edge light is enabled
+         * @hide
+         */
+        public static final String EDGE_LIGHT_ENABLED = "edge_light_enabled";
+
+        /**
+         * Color mode of edge light
+         * @hide
+         */
+        public static final String EDGE_LIGHT_COLOR_MODE = "edge_light_color_mode";
+
+        /**
+         * Custom color (hex value) for edge light
+         * @hide
+         */
+        public static final String EDGE_LIGHT_CUSTOM_COLOR = "edge_light_custom_color";
+
+        /**
          * Whether or not to vibrate when a touchscreen gesture is detected
          * @hide
          */
